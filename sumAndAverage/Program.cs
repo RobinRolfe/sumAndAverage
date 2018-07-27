c0898d68f92d305f89c962848adbdd1323db3cdb
ROBIN ALEXANDER ROLFE <S14001664@mail.glyndwr.ac.uk>
2018-07-27 15:04:04 +0100

    Add project files.

---
+++ sumAndAverage/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace sumAndAverage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            int[] numberArray = new int[10];
+
+            int count = 0;
+            int sum = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Please enter a  number");
+                numberArray.SetValue(Convert.ToInt32(Console.ReadLine()), i);
+            }
+            for (int i = 0; i < numberArray.Length; i++)
+            {
+                count++;
+                Console.WriteLine("number " + count + " is " + numberArray[i]  );
+            }
+            for (int i = 0; i < numberArray.Length; i++)
+            {
+                sum = sum + numberArray[i];
+            }
+
+
+            Console.WriteLine("The sum of the numbers input is " + sum);
+
+            Console.WriteLine("The average of the numbers input is " + sum / numberArray.Length);
+
+
+            Console.ReadLine();
+        }
+
+        
+    }
+}
